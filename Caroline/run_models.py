eeaa71bbb226147fad60dbdab09a1f0a6b8a99ae
cgihlstorf <caroline.gihlstorf@gmail.com>
2023-10-23 20:06:08 -0400

    fix script outputting evaluation metrics

--- Caroline/run_models.py
+++ Caroline/run_models.py
@@ -49,6 +49,12 @@ def test_and_evaluate_model(model, X_test, y_test):
     num_false_positives = 0 #predicted class = 1 and true class = 0
     num_false_negatives = 0 #predicted class = 0 and true class = 1
 
+    accuracy = 0
+    precision = 0
+    recall = 0
+    f_1 = 0
+    auc = 0
+
     for prediction, true_label in zip(predictions, y_test):
         total_examples += 1
         if prediction == 1 and true_label == 1:
@@ -60,24 +66,32 @@ def test_and_evaluate_model(model, X_test, y_test):
         elif prediction == 0 and true_label == 1:
             num_false_negatives += 1
 
+    print("True Positives:", num_true_positives)
+    print("False Negatives:", num_false_negatives)
+    print("False Positives:", num_false_positives)
+    print("True Negatives:", num_true_negatives)
+
     #Calculate Accuracy
-    accuracy = (num_true_positives + num_true_negatives) / (num_true_positives + num_true_negatives + num_false_positives + num_false_negatives)
+    if (num_true_positives + num_true_negatives + num_false_positives + num_false_negatives) != 0:
+        accuracy = (num_true_positives + num_true_negatives) / (num_true_positives + num_true_negatives + num_false_positives + num_false_negatives)
 
     #Calculate Precision
     #fewer false positives is better
-    precision = (num_true_positives) / (num_true_positives + num_false_positives)
+    if (num_true_positives + num_false_positives) != 0:
+        precision = (num_true_positives) / (num_true_positives + num_false_positives)
 
     #Calculate Recall
     #fewer false negatives is better
-    recall = (num_true_positives) / (num_true_positives + num_false_negatives)
+    if (num_true_positives + num_false_negatives) != 0:
+        recall = (num_true_positives) / (num_true_positives + num_false_negatives)
 
     #Calculate F-1
     #TODO verify that this is the correct metric
-    f_1 = (2 * recall * precision) / (recall + precision)
+    if (recall + precision) != 0:
+        f_1 = (2 * recall * precision) / (recall + precision)
 
     #Calculate AUC
     #TODO calculate AUC
-    auc = None
 
     print("Model: ", model)
     print("\t", "Accuracy:", accuracy)
@@ -86,9 +100,9 @@ def test_and_evaluate_model(model, X_test, y_test):
     print("\t", "F-1:", f_1)
     print("\n")
 
-    return model, accuracy, precision, recall, f_1, auc
+    return accuracy, precision, recall, f_1, auc
 
-def train_and_test_logistic_regressions(penalties:list, solvers:list, max_iters:list, output_csv_path, use_test_set:bool):
+def train_and_test_logistic_regressions(penalties:list, solvers:list, max_iters:list, output_csv_path):
     output_csv_file = open(output_csv_path, 'w', newline='')
     output_csv_writer = csv.writer(output_csv_file)
     output_csv_writer.writerow(["Logistic Regression Model", "Dataset", "Penalty", "Solver", "Max Iterations", "Accuracy", "Precision", "Recall", "F-1", "AUC"])
@@ -99,55 +113,66 @@ def train_and_test_logistic_regressions(penalties:list, solvers:list, max_iters:
                 lr = LogisticRegression(penalty=penalty, solver=solver, max_iter=max_iter)
                 lr_model_dataset1 = lr.fit(dataset1_X_train, dataset1_y_train)
                 lr_model_dataset2 = lr.fit(dataset2_X_train, dataset2_y_train)
-
-                #TODO maybe we just don't include test data here because we want to only evaluate one model on the test data
-
-                if use_test_set:
-                    model_1, accuracy_1, precision_1, recall_1, f_1_1, auc_1 = test_and_evaluate_model(lr_model_dataset1, dataset1_X_test, dataset1_y_test)
-                    #model_2, accuracy_2, precision_2, recall_2, f_1_2, auc_2 = test_and_evaluate_model(lr_model_dataset2, dataset2_X_test, dataset2_y_test)
-                else:
-                    model_1, accuracy_1, precision_1, recall_1, f_1_1, auc_1 = test_and_evaluate_model(lr_model_dataset1, dataset1_X_val, dataset1_y_val)
-                    #model_2, accuracy_2, precision_2, recall_2, f_1_2, auc_2 = test_and_evaluate_model(lr_model_dataset2, dataset2_X_val, dataset2_y_val)
+        
+                accuracy_1, precision_1, recall_1, f_1_1, auc_1 = test_and_evaluate_model(lr_model_dataset1, dataset1_X_val, dataset1_y_val)
+                #accuracy_2, precision_2, recall_2, f_1_2, auc_2 = test_and_evaluate_model(lr_model_dataset2, dataset2_X_val, dataset2_y_val)
                 
-                output_csv_writer.writerow([model_1, "1", penalty, solver, max_iter, accuracy_1, precision_1, recall_1, f_1_1, auc_1])
-                #output_csv_writer.writerow([model_2, "2", penalty, solver, max_iter, accuracy_2, precision_2, recall_2, f_1_2, auc_2])
+                output_csv_writer.writerow([lr_model_dataset1, "1", penalty, solver, max_iter, accuracy_1, precision_1, recall_1, f_1_1, auc_1])
+                #output_csv_writer.writerow([lr_model_dataset2, "2", penalty, solver, max_iter, accuracy_2, precision_2, recall_2, f_1_2, auc_2])
                 
     print("Done training and testing Logistic Regression Models!")
 
 
-def train_and_test_svms(Cs:list, kernels:list, degrees:list, gammas:list, output_csv_path, use_test_set:bool):
+def train_and_test_svms(Cs:list, kernels:list, degrees:list, gammas:list, output_csv_path):
     output_csv_file = open(output_csv_path, 'w', newline='')
     output_csv_writer = csv.writer(output_csv_file)
+    output_csv_writer.writerow(["SVM Model", "Dataset", "C", "Kernel", "Degree", "Gamma", "Accuracy", "Precision", "Recall", "F-1", "AUC"])
+
+    for C in Cs:
+        for kernel in kernels:
+            for degree in degrees:
+                for gamma in gammas:
+                    svm = SVC(C=C, kernel=kernel, degree=degree, gamma=gamma)
+                    svm_model_dataset1 = svm.fit(dataset1_X_train, dataset1_y_train)
+                    svm_model_dataset2 = svm.fit(dataset2_X_train, dataset2_y_train)
+                    print("SVM Dataset 2 Accuracy:", svm_model_dataset2.score(dataset2_X_val, dataset2_y_val))
+
+                    #I'm not including the test set as an option here
+
+                    #accuracy_1, precision_1, recall_1, f_1_1, auc_1 = test_and_evaluate_model(svm_model_dataset1, dataset1_X_val, dataset1_y_val)
+                    accuracy_2, precision_2, recall_2, f_1_2, auc_2 = test_and_evaluate_model(svm_model_dataset2, dataset2_X_val, dataset2_y_val)
+
+                    #output_csv_writer.writerow([svm_model_dataset1, "1", C, kernel, degree, gamma, accuracy_1, precision_1, recall_1, f_1_1, auc_1])
+                    output_csv_writer.writerow([svm_model_dataset2, "2", C, kernel, degree, gamma, accuracy_2, precision_2, recall_2, f_1_2, auc_2])
+    
+    print("Done training and testing SVM Models!")
+
 
 
 
 #Data
 dataset1_X_train, dataset1_y_train = get_data("../dataset1_training_data.csv")
 dataset1_X_val, dataset1_y_val = get_data("../dataset1_validation_data.csv")
-dataset1_X_test, dataset2_y_test = get_data("../dataset1_testing_data.csv")
+dataset1_X_test, dataset1_y_test = get_data("../dataset1_testing_data.csv")
 
 dataset2_X_train, dataset2_y_train = get_data("../dataset2_training_data.csv")
 dataset2_X_val, dataset2_y_val = get_data("../dataset2_validation_data.csv")
 dataset2_X_test, dataset2_y_test = get_data("../dataset2_testing_data.csv")
-
-use_test_set = False
     
 #Run Logistic Regression
-penalty_types = ['l2'] #only l2 or no regularization for the default solver. Change solvers?
-solvers = ['lbfgs']
-max_iterations = [2000]
-logistic_regression_validation_output_csv_path = "logistic_regression_validation_results.csv"
-train_and_test_logistic_regressions(penalty_types, solvers, max_iterations, logistic_regression_validation_output_csv_path, use_test_set)
+# penalty_types = ['l2'] #only l2 or no regularization for the default solver. Change solvers?
+# solvers = ['lbfgs']
+# max_iterations = [2000]
+# logistic_regression_validation_output_csv_path = "logistic_regression_validation_results.csv"
+# train_and_test_logistic_regressions(penalty_types, solvers, max_iterations, logistic_regression_validation_output_csv_path)
 
 #Run SVM
-# C = 1.0
-# kernel = 'rbf'
-# degree = 1
-# gamma = 1.0
-# svm = SVC(C=C, kernel=kernel, degree=degree, gamma=gamma)
-# svm_model = svm.fit(dataset2_X_train, dataset2_y_train)
-# accuracy = svm_model.score(dataset2_X_val, dataset2_y_val)
-# print("Accuracy:", accuracy)
+Cs = [1.0]
+kernels = ['rbf']
+degrees = [1]
+gammas = [1.0]
+svm_validation_output_csv_path = "svm_validation_results.csv"
+train_and_test_svms(Cs, kernels, degrees, gammas, svm_validation_output_csv_path)
 
 
 
